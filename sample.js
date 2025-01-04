62a6a8d6874e337b00a0db22dfd41af744fe6ae9
stack-teapot <jakeisagamer20@outlook.com>
2025-01-04 17:32:22 -0500

    Fixed import

--- sample.js
+++ sample.js
@@ -1,6 +1,5 @@
 import { initBuffers } from "./init-buffers.js"
 import { drawScene } from "./draw-scene.js"
-import { cube } from "./cube.js"
 import { LoadOBJ } from "./load_obj.js"
 
 let deltaTime = 0
@@ -39,49 +38,7 @@ async function main() {
   gl.clear(gl.COLOR_BUFFER_BIT)
 
   const vsSource = vertex_shader_text
-
-//   const vsSource = `
-//   attribute vec4 aVertexPosition;
-//   attribute vec3 aVertexNormal;
-//   attribute vec2 aTextureCoord;
-
-//   uniform mat4 uNormalMatrix;
-//   uniform mat4 uModelViewMatrix;
-//   uniform mat4 uProjectionMatrix;
-
-//   varying highp vec2 vTextureCoord;
-//   varying highp vec3 vLighting;
-
-//   void main(void) {
-//     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-//     vTextureCoord = aTextureCoord;
-
-//     // Apply lighting effect
-
-//     highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-//     highp vec3 directionalLightColor = vec3(1, 1, 1);
-//     highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-
-//     highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-
-//     highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-//     vLighting = ambientLight + (directionalLightColor * directional);
-//   }
-// `
-
-    const fsSource = fragment_shader_text
-
-//   const fsSource = `
-//   varying highp vec2 vTextureCoord;
-//   varying highp vec3 vLighting;
-
-//   uniform sampler2D uSampler;
-
-//   void main(void) {
-//     highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
-//     gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
-//   }
-// `
+  const fsSource = fragment_shader_text
 
   // Initialize shader program this is where all the lighting for the vertices is set.
   const shaderProgram = initShaderProgram(gl, vsSource, fsSource)
